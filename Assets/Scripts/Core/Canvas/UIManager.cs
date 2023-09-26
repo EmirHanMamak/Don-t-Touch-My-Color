f318be3b283ff94b67c50f9ff3e95cbdb3400a03
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-09-26 09:45:51 +0300

    SCRIPT- TagList Added, Settings PlayerPrefabs created.

--- Assets/Scripts/Core/Canvas/UIManager.cs
+++ Assets/Scripts/Core/Canvas/UIManager.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Core.Tag;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Core.Canvas
 {
     public class UIManager : MonoBehaviour
-    { 
-    [SerializeField] private Image fadeImage;
-    [SerializeField] private Animator settingsAnimator;
+    {
+        [SerializeField] private Image fadeImage;
+        [SerializeField] private Animator settingsAnimator;
+        private static int CT_OFF = 0, CT_ON = 1;
         private float _fadeOutInTime = 0.5f;
         private int _effectControl = 0;
         /*
@@ -16,9 +20,57 @@ namespace Core.Canvas
          */
         [SerializeField] private GameObject settingsOpen, settingsClose;
         [SerializeField] private GameObject soundOpen, soundClose;
-        [SerializeField] private GameObject vibrationOpen, vibrationClose;
+        [FormerlySerializedAs("vibrationOpen")] [SerializeField] private GameObject hapticOpen;
+        [FormerlySerializedAs("vibrationClose")] [SerializeField] private GameObject hapticClose;
         private string _privacyPolicyUrl = "https://www.emirhanmamak.com/privacy-policy/";
         private string _termsOfUse = "https://www.emirhanmamak.com/term-of-use/";
+
+        private void Start()
+        {
+            CheckHasKey();
+
+            CheckConditions();
+        }
+
+        private void CheckConditions()
+        {
+            /*
+             * Sound
+             */
+            if (PlayerPrefs.GetInt(TagList.PP_Sound) == CT_ON)
+            {
+                SoundButtonOff();
+            }
+            else if (PlayerPrefs.GetInt(TagList.PP_Sound) == CT_OFF)
+            {
+                SoundButtonOn();
+            }
+            /*
+             * Haptic 
+             */
+            if (PlayerPrefs.GetInt(TagList.PP_Haptic) == CT_ON)
+            {
+                HapticButtonOn();
+            }
+            else if (PlayerPrefs.GetInt(TagList.PP_Haptic) == CT_OFF)
+            {
+                HapticButtonOff();
+            }
+        }
+
+        private static void CheckHasKey()
+        {
+            if (PlayerPrefs.HasKey(TagList.PP_Sound) == false)
+            {
+                PlayerPrefs.SetInt(TagList.PP_Sound, CT_ON);
+            }
+
+            if (PlayerPrefs.HasKey(TagList.PP_Haptic) == false)
+            {
+                PlayerPrefs.SetInt(TagList.PP_Haptic, CT_ON);
+            }
+        }
+
         public void PrivacyPolicyClick()
         {
             Application.OpenURL(_privacyPolicyUrl);
@@ -31,21 +83,27 @@ namespace Core.Canvas
         {
             soundOpen.SetActive(false);
             soundClose.SetActive(true);
+            PlayerPrefs.SetInt(TagList.PP_Sound, CT_OFF);
+            AudioListener.volume = 0;
         }
         public void SoundButtonOff()
         {
             soundOpen.SetActive(true);
             soundClose.SetActive(false);
+            PlayerPrefs.SetInt(TagList.PP_Sound, CT_ON);
+            AudioListener.volume = 1;
         }        
-        public void VirbationButtonOn()
+        public void HapticButtonOn()
         {
-            vibrationOpen.SetActive(false);
-            vibrationClose.SetActive(true);
+            hapticOpen.SetActive(false);
+            hapticClose.SetActive(true);
+            PlayerPrefs.SetInt(TagList.PP_Haptic, CT_ON);
         }
-        public void VirbationButtonOff()
+        public void HapticButtonOff()
         {
-            vibrationOpen.SetActive(true);
-            vibrationClose.SetActive(false);
+            hapticOpen.SetActive(true);
+            hapticClose.SetActive(false);
+            PlayerPrefs.SetInt(TagList.PP_Haptic, CT_OFF);
         }
         public void SettingsButonOpen()
         {
---
+++ Assets/Scripts/Core/Tag/TagList.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Tag
+{
+    public class TagList
+    {
+        public static string PP_Sound = "Sound";
+        public static string PP_Haptic = "Haptic";
+    }
+}
