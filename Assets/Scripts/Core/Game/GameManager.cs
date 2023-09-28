c43bc6333c6dad474e82136b639319f7a4625dda
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-09-28 12:40:41 +0300

    SCPRITS- GoogleAds Files added. - Finish Panel Reward Ads added, Banner Ads Added.

--- Assets/Scripts/Core/Game/GameManager.cs
+++ Assets/Scripts/Core/Game/GameManager.cs
@@ -3,15 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Core;
 using Core.Tag;
+using GoogleMobileAds.Api;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private RewardedAds _rewardedAds;
+    [SerializeField] private InterstitialAds _interstitialAds;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(TagList.Player) && this.gameObject.CompareTag(TagList.FinishLine))
         {
             Variables.GameCondition = Variables.GC_NEXTLEVEL;
+            _rewardedAds.LoadRewardedAd();
+            //_interstitialAds.LoadLoadInterstitialAd();
             Variables.firstTouch = 0;
         }
     }
